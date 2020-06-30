9acd55f714fcdd944cd2822a96b773b5f60c8558
pushkar <Pushkar_Narayan@Outlook.Com>
2020-06-30 14:26:37 +0530

    changes done

--- MobileAutomationFramework/src/test/java/core/DriverManager.java
+++ MobileAutomationFramework/src/test/java/core/DriverManager.java
@@ -1,20 +1,17 @@
 package core;
 
-import org.apache.log4j.Logger;
-
-
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 
 public class DriverManager extends BasePage{
-	Logger log = Logger.getLogger("devpinoyLogger");
+	
 	private AndroidDriver<MobileElement> driver= null;
 	
 
 	public AndroidDriver<MobileElement> setDriver()
 	{
 	    driver =launchDriver();
-	    log.debug("initiating driver");
+	 
 		System.out.println("initiating driver");
 		return driver;
 		
